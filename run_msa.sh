2ef15b9da1978501d798b4c8637f53f915b1a4b2
Harshini <harshini_venkatachalam@brown.edu>
2022-05-13 01:14:39 -0400

    results for p53

---
+++ run_msa.sh
@@ -0,0 +1,11 @@
+# assuming you have installed t-coffee command line for mac - comment out if unneeded
+export PATH=/Users/harshini/.t_coffee/bin/macosx:$PATH
+export PLUGINS_4_TCOFFEE=/Users/harshini/.t_coffee/plugins/macosx:
+ 
+# t_coffee 
+
+
+# clustal omega
+
+
+# 
